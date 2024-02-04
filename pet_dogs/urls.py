93f6247f5ea2786d5576a6ebca5175b4fdafed50
Divyanshu <divyanshukrishna11@gmail.com>
2024-02-04 18:56:29 +0530

    gunicorn_added

--- pet_dogs/urls.py
+++ pet_dogs/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from myapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name="home"),
     path('adoptions/<int:pet_id>/',views.pet_detail,name="pet_detail"),
     
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
